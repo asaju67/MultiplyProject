7cd67f345ae2fdc4ebbf8fd31ec859167a16a05e
Ashley Saju <asaju2363@stu.d214.org>
2020-11-06 12:09:40 -0800

    Initial Commit

---
+++ MultiplyProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiplyProject
+//
+//  Created by user172370 on 11/6/20.
+//  Copyright © 2020 user172370. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MultiplyProjectTests/MultiplyProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  MultiplyProjectTests.swift
+//  MultiplyProjectTests
+//
+//  Created by user172370 on 11/6/20.
+//  Copyright © 2020 user172370. All rights reserved.
+//
+
+import XCTest
+@testable import MultiplyProject
+
+class MultiplyProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
