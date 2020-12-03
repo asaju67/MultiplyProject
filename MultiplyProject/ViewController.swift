0f57fb494be9eb9d38784e38417967872fae02da
Ashley Saju <asaju2363@stu.d214.org>
2020-12-03 12:41:14 -0800

    Full project

--- MultiplyProject/ViewController.swift
+++ MultiplyProject/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textfield1: UITextField!
+    @IBOutlet weak var textfield2: UITextField!
+    @IBOutlet weak var answer: UILabel!
+    @IBOutlet weak var even: UIImageView!
+    
+    @IBOutlet weak var yoshi: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        
     }
 
+    @IBAction func whenButtonPressed(_ sender: Any) {
+        let number1 = Int(textfield1.text!)!
+        let number2 = Int(textfield2.text!)!
+        answer.text = String(number1 * number2)
+        textfield2.resignFirstResponder()
+        textfield1.resignFirstResponder()
+        if (number2 * number1) == 64 {
+            yoshi.image = UIImage(named: "yoshi-1")
+        }else{
+            yoshi.image = UIImage()
+        }
+        if (number2 * number1) % 2 == 1{
+            even.image = UIImage (named: "SillyMonaLisa")
+        }else{
+            even.image = UIImage (named: "cat")
 
+        }
+        
+    }
+    @IBAction func Reset(_ sender: Any) {
+        even.image = UIImage()
+        yoshi.image = UIImage()
+    }
+    
 }
 
